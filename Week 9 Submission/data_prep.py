340bf2c7cd92931beaa658c9187053dbb1d1da2e
udbhavbalaji <udbhavbalaji@gmail.com>
2021-08-07 19:49:17 +0530

    Started EDA for the repo

--- Week 9 Submission/data_prep.py
+++ Week 9 Submission/data_prep.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def clean_data(filename, target_filename, sep, na):
     # Reading in the full data
@@ -21,8 +22,33 @@ def clean_data(filename, target_filename, sep, na):
             column.append(col)
     df.columns = column
 
+    # Cleaning out the data as per decisions taken in Week 8
+
+    ## Imputing NaN Values
+
+    ### Column: JOB ; Method of Imputing NaN Values: Mode
+    df['job'] = df['job'].fillna(df['job'].mode()[0])
+
+    ### Column: MARITAL ; Method of Imputing NaN Values: Mode
+    df['marital'] = df['marital'].fillna(df['marital'].mode()[0])
+
+    ### Column: EDUCATION ; Method of Imputing NaN Values: Creating a N/A category because there are a lot of missing values
+    df['education'].fillna(na, inplace=True)
+
+    ### Column: DEFAULT ; Method of Imputing NaN Values: Mode
+    df['default'] = df['default'].fillna(df['default'].mode()[0])
+
+    ### For HOUSING and LOAN columns, we can just drop the rows with NaN values as it would not affect the dataset too much
+    df.dropna(axis=0, how='any', inplace=True)
+
+    ## Replacing the 999 value in PDAYS with a 0
+    df['pdays'] = df['pdays'].replace([999],0)
+
+    ## Dropping duplicates
+    df.drop_duplicates(keep='first', inplace=True)
+
     # Writing the dataframe into a new cleaned up data_set that we can use
     df.to_csv(target_filename, index=False)
 
 if __name__ == "__main__":
-    clean_data('bank_additional_full.csv','bank_data.csv',';','unknown')
+    clean_data('Data/bank_additional_full.csv','Data/bank_data.csv',';','unknown')
